a77b221548f72861c1225eaf8a23ea9661028696
Geetanjali <geetbgs04@gmail.com>
2023-05-29 02:47:23 +0530

    Added Bloglist component, fetched content from json file

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,27 @@
-import { useState } from "react";
+import { useState ,useEffect} from "react";
+import Bloglist from "./Bloglist";
+import useFetch from "./usefetch";
 
 const Home = () => {
-    const[blogs,setBlogs]=useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+const {data:blogs, isPending,error} =useFetch('http://localhost:8000/blogs')
+    // const[name,setName]=useState('mario');
+
+    // const handleDelete=(id)=>{
+    //     const newBlogs=blogs.filter(blog=> blog.id!== id)
+    //     setBlogs(newBlogs);
+    // }
+    // handleDelete={handleDelete}, we were first passing this as prop but it deleted the data only at the frontend
+// fetch data , for authentication
+    
 
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p> Written by {blog.author}.</p>
-                </div>
-            ))}
+            {error && <div>{error}</div>}
+            { isPending && <div>Loading..</div>}
+             {blogs && <Bloglist blogs={blogs} title='All blogs !' />}
+            {/* blogs && <Bloglist blogs={blogs.filter((blog)=>blog.author==='mario')} title="Mario's blogs"/> */}
+            {/* <button onClick={()=>setName('luigi')}>Change name</button>
+            <p>{name}</p> */}
         </div>
      );  
 }
