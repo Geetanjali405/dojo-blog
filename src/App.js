ac84f8fd6201de1621abaa814a1f93c5c1d855b5
Geetanjali <geetbgs04@gmail.com>
2023-05-28 05:32:01 +0530

    Added blogs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
-import './App.css';
-import Navbar from './Navbar.js';
+import './index';
+import Navbar from './navbar';
+import Home from './Home';
+
 
 function App() {
-  const title='Welcome to my new blog ';
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
       </div>
     </div>
   );
